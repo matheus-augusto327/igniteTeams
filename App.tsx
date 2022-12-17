13a695ecee800b6713e7226b73111a73d7ec4fe7
Matheus <matheus.augustoreal@gmail.com>
2022-12-16 21:07:39 -0300

    feat: added custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from "react-native";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
