460d93e6d4f537f09d29913450811243d8d051fd
Matheus <matheus.augustoreal@gmail.com>
2022-12-16 21:16:01 -0300

    feat: styled status bar

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "react-native";
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
@@ -13,6 +14,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
