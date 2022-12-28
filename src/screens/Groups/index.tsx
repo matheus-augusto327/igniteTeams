9d27619dc7483d29b1b837577f77dd7b9ee399df
Matheus <matheus.augustoreal@gmail.com>
2022-12-28 08:28:25 -0300

    feat: navigation

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,19 @@ import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { ListEmpty } from "@components/ListEmpty";
+import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Galera da Rocketseat"]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -26,7 +33,7 @@ export function Groups() {
         showsVerticalScrollIndicator={false}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
